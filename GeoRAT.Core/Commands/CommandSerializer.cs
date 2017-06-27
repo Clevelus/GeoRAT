cb9c337019bd90dfc0dc46e013782b4b7b4a61e1
kek\Alan <A@A>
2017-06-27 11:27:20 -0700

    no message

--- GeoRAT.Core/Commands/CommandSerializer.cs
+++ GeoRAT.Core/Commands/CommandSerializer.cs
@@ -13,20 +13,29 @@ namespace GeoRAT.Core.Commands
 
      public byte[] Serialize(Commands cmd)
         {
-            MemoryStream stream = new MemoryStream();
-            BinaryWriter writer = new BinaryWriter(stream);
-            writer.Write(cmd.CommandType);
-            writer.Write(cmd.CommandParams);
-            return stream.ToArray();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write(cmd.CommandType);
+                    writer.Write(cmd.CommandParams);
+                }
+                return stream.ToArray();
+            }
+
         }
 
        public Commands Deserialize(byte[] cmdbuf)
         {
             Commands command = new Commands();
-            MemoryStream stream = new MemoryStream(cmdbuf);
-            BinaryReader read = new BinaryReader(stream);
-            command.CommandType = read.ReadString();
-            command.CommandParams = read.ReadString();
+            using (MemoryStream stream = new MemoryStream(cmdbuf))
+            {
+                using (BinaryReader read = new BinaryReader(stream))
+                {
+                   command.CommandType = read.ReadString();
+                   command.CommandParams = read.ReadString();
+                }
+            }
             return command;
         }
     }
